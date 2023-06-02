b57d17fb847546554af35adac1d64d9624b25661
ehtluhg <dy.gabrielrhobert@auf.edu.ph>
2023-06-02 13:00:55 +0800

    first commit

---
+++ pdc03_module07/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using pdc03_module07.Services;
+using pdc03_module07.View;
+
+namespace pdc03_module07
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new View.ShowEmployeePage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ pdc03_module07/Services/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pdc03_module07.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+
+namespace pdc03_module07.Services
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<EmployeeModel> Employees { get; set; }
+        public DatabaseContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Employee.db");
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
+    }
+}
